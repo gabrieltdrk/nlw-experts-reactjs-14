8b5847f1fa6953a70c89268d775a5e9b57e29f49
Gabriel Andrade <gabriel.andradesx@gmail.com>
2024-02-09 18:25:07 -0300

    deploy completo no projeto

--- src/app.tsx
+++ src/app.tsx
@@ -44,8 +44,17 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+  
+    setNotes(notesArray);
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
       <form className="w-full">
         <input
@@ -59,10 +68,10 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated}/>
         {filteredSearch.map(note => {
-          return <NoteCard key={note.id} note={note}  />
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}  />
         })}
       </div>
     </div>
